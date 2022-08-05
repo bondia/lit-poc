ded1d3c2e9890f1798b6965e551a2ed5f8fa073e
Marius Nogueras <bondia@gmail.com>
2022-08-05 13:07:25 +0200

    reset column push

--- src/components/Grid/Column.ts
+++ src/components/Grid/Column.ts
@@ -4,20 +4,35 @@ import ColumnStyles from './ColumnStyles';
 
 @customElement('wd-column')
 export class Column extends LitElement {
-  @property({ type: String })
-  xs: String = '1';
+  @property({ type: Number })
+  xs: Number = 1;
 
-  @property({ type: String })
-  sm: String = '1';
+  @property({ type: Number })
+  sm: Number = 1;
 
-  @property({ type: String })
-  md: String = '1';
+  @property({ type: Number })
+  md: Number = 1;
 
-  @property({ type: String })
-  lg: String = '1';
+  @property({ type: Number })
+  lg: Number = 1;
 
-  @property({ type: String })
-  xl: String = '1';
+  @property({ type: Number })
+  xl: Number = 1;
+
+  @property({ type: Number })
+  pushxs: Number = 0;
+
+  @property({ type: Number })
+  pushsm: Number = 0;
+
+  @property({ type: Number })
+  pushmd: Number = 0;
+
+  @property({ type: Number })
+  pushlg: Number = 0;
+
+  @property({ type: Number })
+  pushxl: Number = 0;
 
   render() {
     return html`
--- src/components/Grid/ColumnStyles.ts
+++ src/components/Grid/ColumnStyles.ts
@@ -25,14 +25,27 @@ const withMediaQuery = (breakpoint: Breakpoint, styles: string): string => {
   return styles;
 };
 
+const widthRule = (breakpoint: Breakpoint, columns: number): string =>
+  `
+    :host([${breakpoint}='${columns}']) {
+        width: calc(100% / 12 * ${columns} - 20px);
+    }
+  `;
+
+const pushRuleMargin = (columns: number): string =>
+  columns > 0
+    ? `margin-left: calc(100% / 12 * ${columns} + 10px);`
+    : 'margin-left: 10px';
+
 const createRule = (breakpoint: Breakpoint, columns: number): string => `
-  :host([${breakpoint}='${columns}']) {
-    width: calc(100% / 12 * ${columns} - 20px);
+  ${columns != 0 ? widthRule(breakpoint, columns) : ''}
+  :host([push${breakpoint}='${columns}']) {
+    ${pushRuleMargin(columns)}
   }
 `;
 
 const buildBreakpointRules = (breakpoint: Breakpoint): string =>
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].reduce(
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].reduce(
     (prev, next) => prev + createRule(breakpoint, next),
     ''
   );
